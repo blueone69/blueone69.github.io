7d95ee8b9647a24f489a366ca60cb06074c71ae8
blueone69 <felix.holm.1989@gmail.com>
2022-07-15 09:50:49 +0200

    dev branch
    Testing REACT app structure

---
+++ app.js
@@ -0,0 +1,22 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="name">
+        <h1>Felix Holm</h1>
+      </div>
+      <img src={require("/src/profile.jpg")} alt="photograph"></img>
+      <div className="description">
+        <h2>
+          Hello and welcome to my website checkout the links down below for more
+          information.
+        </h2>
+      </div>
+      <div className="button">LinkedIn</div>
+      <div className="button">Facebook</div>
+      <div className="button">Resume</div>
+      <div className="button">Portfolio</div>
+    </div>
+  );
+}
